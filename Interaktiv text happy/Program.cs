18231cde792402c568968849fc9f82c211c61f98
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-08-30 10:52:05 +0200

    fläskfile

--- Interaktiv text happy/Program.cs
+++ Interaktiv text happy/Program.cs
@@ -1,4 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿Console.WriteLine("Användarnamn");
+String name = Console.ReadLine();
 
+if(name == "Ludde"){
+System.Console.WriteLine("Välkommen in kanske.");
+}
+else{
+    System.Console.WriteLine("dra åt skogen");
+}
 Console.ReadLine();
+
+String input = Console.ReadLine();
+if(input == "hej då"){
+    return;
+}
